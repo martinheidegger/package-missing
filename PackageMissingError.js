8444807eaa466e8b538bfcdeec3224fa1ecd8efb
Martin Heidegger <martin.heidegger@gmail.com>
2016-09-05 10:51:50 +0900

    Reduced file-size by referencing ok/equal directly

--- PackageMissingError.js
+++ PackageMissingError.js
@@ -1,6 +1,8 @@
 'use strict'
 
 var assert = require('assert')
+var ok = assert.ok
+var equal = assert.equal
 var MANY_MSG = 'ONE of the following commands:\n'
 var ONE_MSG = 'the following command:\n$ npm install --save '
 
@@ -8,13 +10,13 @@ function PackageMissingError (task, recommendedPackages) {
   if (!(this instanceof PackageMissingError)) {
     return new PackageMissingError(task, recommendedPackages)
   }
-  assert.equal(typeof task, 'string', 'Task (arg#0) needs to be a string.')
+  equal(typeof task, 'string', 'Task (arg#0) needs to be a string.')
   task = task.trim()
-  assert.ok(task.length > 0, 'The task (arg#0) may not be empty.')
-  assert.ok(Array.isArray(recommendedPackages), 'The recommended packages (arg#1) has to be an array.')
-  assert.ok(recommendedPackages.length > 0, 'The recommended packages (arg#1) may not be empty.')
+  ok(task.length > 0, 'The task (arg#0) needs to contain something.')
+  ok(Array.isArray(recommendedPackages), 'The recommended packages (arg#1) has to be an array.')
+  ok(recommendedPackages.length > 0, 'The recommended packages (arg#1) may not be empty.')
   recommendedPackages.forEach(function (pkg, index) {
-    assert.equal(typeof pkg, 'string', 'The recommended package (arg#1#' + index + ': ' + pkg + ' has to be a string.')
+    equal(typeof pkg, 'string', 'The recommended package (arg#1#' + index + ': ' + pkg + ' has to be a string.')
   })
 
   this.code = 'EPACKAGEMISSING'
